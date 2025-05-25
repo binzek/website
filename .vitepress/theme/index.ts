532f79d0afe11e44e570017ae96970adf5d80243
Abdul Wajid <abdulwajidnv@gmail.com>
2025-05-25 12:00:04 +0530

    Setup Vercel Web Analytics

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,12 @@
 import DefaultTheme from "vitepress/theme";
+import { EnhanceAppContext } from "vitepress";
+import { inject } from "@vercel/analytics";
 import "@catppuccin/vitepress/theme/mocha/rosewater.css";
 
-export default DefaultTheme;
+export default {
+  extends: DefaultTheme,
+  enhanceApp(ctx: EnhanceAppContext) {
+    DefaultTheme.enhanceApp(ctx);
+    inject();
+  },
+};
