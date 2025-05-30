825e8265fe2ba5d5e9d33b6ed8eceba38e794262
Abdul Wajid <abdulwajidnv@gmail.com>
2025-05-30 21:36:33 +0530

    Add font for Malayalam

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,9 @@
-import DefaultTheme from "vitepress/theme";
+import DefaultTheme from "vitepress/theme-without-fonts";
 import { EnhanceAppContext } from "vitepress";
 import { inject } from "@vercel/analytics";
 import { injectSpeedInsights } from "@vercel/speed-insights";
 import "@catppuccin/vitepress/theme/mocha/rosewater.css";
+import "./fonts.css";
 
 export default {
   extends: DefaultTheme,
